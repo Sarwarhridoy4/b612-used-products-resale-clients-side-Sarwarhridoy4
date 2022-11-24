1b071279c5a93af50773414e59785c00d33943d9
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-24 10:00:22 +0600

    footer added

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,46 +1,71 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/Logo/logo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/Logo/logo.png";
 
 const Navbar = () => {
-    //menu-items goes here//
+  //menu-items goes here//
 
-    const menuItems = <React.Fragment>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">SignUp</Link></li>
-        <li><Link to="/blogs">Blogs</Link></li>
-        
+  const menuItems = (
+    <React.Fragment>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      <li>
+        <Link to='/signup'>SignUp</Link>
+      </li>
+      <li>
+        <Link to='/blogs'>Blogs</Link>
+      </li>
     </React.Fragment>
+  );
 
-    return (
-        <div className="navbar bg-green-300">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        {menuItems}
-      </ul>
-                </div>
-                <img src={logo} alt="logo" className='w-16' />
-    <Link to='/' className="btn btn-ghost normal-case text-xl">UsedMobileDeal</Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal p-0">
-      {menuItems}
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <div className="avatar online">
-  <div className="w-12 rounded-full">
-    <img src="https://placeimg.com/192/192/people" alt='' />
-  </div>
-</div>
-  </div>
-</div>
-    );
+  return (
+    <div className='navbar bg-green-300'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+          >
+            {menuItems}
+          </ul>
+        </div>
+        <img src={logo} alt='logo' className='w-16' />
+        <Link to='/' className='btn btn-ghost normal-case text-xl'>
+          UsedMobileDeal
+        </Link>
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal p-0'>{menuItems}</ul>
+      </div>
+      <div className='navbar-end'>
+        <div className='avatar online'>
+          <div className='w-12 rounded-full'>
+            <img src='https://placeimg.com/192/192/people' alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
