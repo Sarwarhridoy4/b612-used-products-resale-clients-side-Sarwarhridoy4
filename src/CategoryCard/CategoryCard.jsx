feddc63ba259b48f14b6d095e1cdb7ec5dda13b6
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-24 22:13:50 +0600

    firebase config added

--- src/CategoryCard/CategoryCard.jsx
+++ src/CategoryCard/CategoryCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CategoryCard = ({ category }) => {
   const { name, image } = category;
@@ -15,7 +16,7 @@ const CategoryCard = ({ category }) => {
           {name}
         </h3>
         <span className='mt-1 font-medium text-gray-600 dark:text-gray-300'>
-          <button className='btn btn-sm btn-success'>Expolre</button>
+          <Link to='/ '><button className='btn btn-sm btn-success'>Expolre</button></Link>
         </span>
       </div>
     </div>
