4150c47b0e31dd5514255213437d00f181312a7b
Sarwarhridoy4 <sarwarhidoy4@gmail.com>
2023-03-15 11:23:58 +0600

    button name changed

--- src/Components/Contact/Contact .jsx
+++ src/Components/Contact/Contact .jsx
@@ -49,7 +49,7 @@ const Contact = () => {
               ></textarea>
             </div>
             <button class='text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
-              Button
+              Submit
             </button>
             <p class='text-xs text-gray-500 mt-3'>
               Chicharrones blog helvetica normcore iceland tousled brook viral
